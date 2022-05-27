1659183fea8e93d454fa64279c73b9ef4e800f05
guk, mu <garuru7080@gmail.com>
2022-05-27 16:56:12 +0900

    login div add

--- src/App.js
+++ src/App.js
@@ -13,6 +13,16 @@ function App() {
       <div className='web-logo'>
         로스트아크 길드 매니저
       </div>
+
+      <div className='login-form'>
+        <input className='id' type='email' placeholder='이메일을 입력해주세요.'/>
+        <input className='pw' type='password' placeholder='비밀번호를 입력해주세요.'/>
+        <button className='btn'>확인</button>
+      </div>
+
+      <div>
+        <button className='btn'>회원가입</button>
+      </div>
     </div>
   );
 }
