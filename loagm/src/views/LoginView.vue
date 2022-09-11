acaeec2eb49c9bd5d936a975eae6fac33432ef4c
DESKTOP-LOSKL2P\A <garuru7080@gmail.com>
2022-09-11 16:54:02 +0900

    LoginView html setting

--- loagm/src/views/LoginView.vue
+++ loagm/src/views/LoginView.vue
@@ -1,6 +1,25 @@
 <template>
+  <!-- 회원가입 선택 모달창 -->
+  <div v-if="signInBtn == true">
+    <button>저는 <span>길드장</span>이에요!</button>
+    <button>저는 <span>길드원</span>이에요!</button>
+  </div>
+
+  <!-- 메인 로그인 페이지html -->
   <div>
     <h1>로스트아크 길드 매니저</h1>
+  </div>
+  <div>
+    <div>
+      <input type="text" id="userId">
+      <input type="text" id="userPw">
+    </div>
+    <div>
+      <button id="logInBtn">확인</button>
+    </div>
+  </div>
+  <div>
+    <button id="signInBtn">회원가입</button>
   </div>
 </template>
 
@@ -8,7 +27,7 @@
 export default {
   data () {
     return {
-      value: 0
+      signInBtn: false
     }
   }
 }
